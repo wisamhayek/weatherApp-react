39515ca7039fbd750e0b523491e1b30e9594baf4
Wisam Hayek <hayekwisam@gmail.com>
2022-04-20 22:18:11 -0700

    Project Done

--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -1,9 +1,8 @@
 import { createContext } from "react";
 
-const WeatherContext = createContext({
-    cities: [],
-    //Function to add to current array.
-    addCity: (name, temperature, description, feelsLike, WindSpeed, imgIcon) => { },
-  });
+const WeatherContext = createContext(
+    // cities: [],
+    // addCity: (name, temperature, description, feelsLike, WindSpeed, imgIcon) => { },
+  );
 
 export default WeatherContext;
